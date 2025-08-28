7a2b8053b977f474efac8cc1151345295dcda743
arionas188 <arionaskonstantinos@me.com>
2025-08-28 18:30:43 +0300

    projects and pharmacy project

--- index.js
+++ index.js
@@ -16,9 +16,24 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-function updateWidth() {
-    document.body.setAttribute('data-width', window.innerWidth);
-  }
-  window.addEventListener('resize', updateWidth);
-  updateWidth();
 
+// typewriter h1 , p sto index.html
+function typeWriter(elementId, text, speed, callback) {
+    let i = 0;
+    function typing() {
+      if (i < text.length) {
+        document.getElementById(elementId).innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing, speed);
+      } else {
+        if (callback) callback(); // μόλις τελειώσει → ξεκινάει το επόμενο
+      }
+    }
+    typing();
+  }
+  
+  // πρώτα γράφεται το h1
+  typeWriter("typewriter-h1", "WorkRoom", 220, function() {
+    // μετά γράφεται το p
+    typeWriter("typewriter-p", "Architecture & Design", 100);
+  });
