a9ece5e75622e92443de0d608268c75e5d131df1
arionas188 <arionaskonstantinos@me.com>
2025-09-23 20:17:51 +0300

    projects and pharmacy project

--- js/libraries/cookieconsent-config.js
+++ js/libraries/cookieconsent-config.js
@@ -1,60 +1,61 @@
 import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.1.0/dist/cookieconsent.umd.js';
 
-/**
- * All config. options available here:
- * https://cookieconsent.orestbida.com/reference/configuration-reference.html
- */
 CookieConsent.run({
-
-    categories: {
-        necessary: {
-            enabled: true,  // this category is enabled by default
-            readOnly: true  // this category cannot be disabled
-        },
-        analytics: {}
+  guiOptions: {
+    consentModal: {
+      layout: 'cloud',               // bar, box, cloud
+      position: 'bottom center',   // θέση: top, bottom, left, right, center
+      flipButtons: false
     },
-
-    language: {
-        default: 'en',
-        translations: {
-            en: {
-                consentModal: {
-                    title: 'We use cookies',
-                    description: 'Cookie modal description',
-                    acceptAllBtn: 'Accept all',
-                    acceptNecessaryBtn: 'Reject all',
-                    showPreferencesBtn: 'Manage Individual preferences'
-                },
-                preferencesModal: {
-                    title: 'Manage cookie preferences',
-                    acceptAllBtn: 'Accept all',
-                    acceptNecessaryBtn: 'Reject all',
-                    savePreferencesBtn: 'Accept current selection',
-                    closeIconLabel: 'Close modal',
-                    sections: [
-                        {
-                            title: 'Somebody said ... cookies?',
-                            description: 'I want one!'
-                        },
-                        {
-                            title: 'Strictly Necessary cookies',
-                            description: 'These cookies are essential for the proper functioning of the website and cannot be disabled.',
-
-                            //this field will generate a toggle linked to the 'necessary' category
-                            linkedCategory: 'necessary'
-                        },
-                        {
-                            title: 'Performance and Analytics',
-                            description: 'These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.',
-                            linkedCategory: 'analytics'
-                        },
-                        {
-                            title: 'More information',
-                            description: 'For any queries in relation to my policy on cookies and your choices, please <a href="#contact-page">contact us</a>'
-                        }
-                    ]
-                }
-            }
-        }
+    preferencesModal: {
+      layout: 'box',               // box ή bar
+      position: 'right'            // left ή right
     }
+  },
+
+  categories: {
+    necessary: {
+      enabled: true,
+      readOnly: true
+    },
+    analytics: {}
+  },
+
+  language: {
+    default: 'el',
+    translations: {
+      el: {
+        consentModal: {
+          title: 'Χρησιμοποιούμε cookies',
+          description: 'Τα cookies μάς βοηθούν να βελτιώσουμε την εμπειρία σας. Μπορείτε να αποδεχτείτε όλα ή να επιλέξετε τις προτιμήσεις σας.',
+          acceptAllBtn: 'Αποδοχή όλων',
+          acceptNecessaryBtn: 'Απόρριψη μη απαραίτητων',
+          showPreferencesBtn: 'Διαχείριση προτιμήσεων'
+        },
+        preferencesModal: {
+          title: 'Διαχείριση προτιμήσεων cookies',
+          acceptAllBtn: 'Αποδοχή όλων',
+          acceptNecessaryBtn: 'Απόρριψη μη απαραίτητων',
+          savePreferencesBtn: 'Αποθήκευση επιλογών',
+          closeIconLabel: 'Κλείσιμο παραθύρου',
+          sections: [
+            {
+              title: 'Απαραίτητα cookies',
+              description: 'Αυτά τα cookies είναι αναγκαία για τη σωστή λειτουργία του ιστότοπου και δεν μπορούν να απενεργοποιηθούν.',
+              linkedCategory: 'necessary'
+            },
+            {
+              title: 'Στατιστικά & Analytics',
+              description: 'Τα cookies αυτά μας επιτρέπουν να κατανοούμε πώς χρησιμοποιείται ο ιστότοπος και να βελτιώνουμε τις υπηρεσίες μας.',
+              linkedCategory: 'analytics'
+            },
+            {
+              title: 'Περισσότερες πληροφορίες',
+              description: 'Για απορίες σχετικά με την πολιτική cookies, παρακαλούμε <a href="#contact-page">επικοινωνήστε μαζί μας</a>.'
+            }
+          ]
+        }
+      }
+    }
+  }
 });
