b788a24c49451a9ec4dda74004011a4f943d61ca
arionas188 <arionaskonstantinos@me.com>
2025-10-11 20:10:32 +0300

    performance priority1  upd14

--- js/faceId.js
+++ js/faceId.js
@@ -1,6 +1,7 @@
 // START Face id for circle photo - section our team - 
 async function centerFace() {
-    await faceapi.nets.tinyFaceDetector.loadFromUri('/models'); // φόρτωσε τα μοντέλα
+    // Φορτώνει τα μοντέλα από CDN αντί για local server (αποφεύγει 404 errors)
+    await faceapi.nets.tinyFaceDetector.loadFromUri('https://cdn.jsdelivr.net/npm/@vladmandic/face-api/model');
     const img = document.getElementById('profile-img');
   
     const detections = await faceapi.detectSingleFace(img, new faceapi.TinyFaceDetectorOptions());
